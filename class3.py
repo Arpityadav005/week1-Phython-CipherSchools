89050c5adeab0582067ee6882fb8976b88c843bd
Arpityadav005 <119453420+Arpityadav005@users.noreply.github.com>
2022-12-05 22:44:28 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,11 @@
+
+
+print("Arpit Yadav")
+print(1)
+# print can accept infinite number of elements.
+print(1, 2, 3, 4, 5, 6, 7, sep=",,")
+print(1, 2, 3, "Arpit", 3.4, 1 + 5j, sep=",,")
+print("Hello")
+print("World !")
+print("Hello", end=";")
+print("World !")
